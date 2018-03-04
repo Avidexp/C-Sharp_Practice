de9ffd0fe02dd64231a7f3b7c8cbf695d8c2d8a4
Dylan Jackson <dylan.jackson@globaltranz.com>
2018-03-04 11:33:54 -0700

    C# Practice

--- PrivateDataMembers/PrivateDataMembers/Program.cs
+++ PrivateDataMembers/PrivateDataMembers/Program.cs
@@ -2,11 +2,106 @@
 
 namespace PrivateDataMembers
 {
+    class Time
+    {
+        private int hours, minutes, seconds;  // only accessible through class TIme
+        
+        public Time(int h, int m, int s)
+        {
+            hours = h;
+            minutes = m;
+            seconds = s;
+        }
+
+        public Time(int h, int m)
+        {
+            hours = h;
+            minutes = m;
+            seconds = 0;
+        }
+        public Time(int h)
+        {
+            hours = h;
+            minutes = 0;
+            seconds = 0;
+        }
+        private int hour, minute, second;
+
+        public Time() // Default constructor - Not required 
+        {
+            hour = 0;
+            minute = 0;
+            second = 0; 
+        }
+        // properties 
+
+        public int Hour // Property Hour
+        {
+            get
+            {
+                return hour;
+            }
+            set
+            {
+                hour = value;
+            }
+        }
+
+        public int Minute // Property Hour
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                minute = value;
+            }
+        }
+
+        public int Second // Property Hour
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                second = value;
+            }
+        }
+
+        public void setTime(int h, int m, int s)
+        {
+            hour = h;
+            minute = m;
+            second = s;
+        }
+
+        public override String ToString() //override - yes I know this method already exists, I like mine better and want to use mine instead.
+        {
+            return hour + ":" + minute + ":" + second;
+        }
+
+        public String displayTime()
+        {
+            return hour + ":" + minute + ":" + second;
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Time theTime = new Time();
+
+
+            theTime.setTime(12, 23, 0);
+            Console.WriteLine("The time is " + theTime.displayTime());
+            Object obj = new object();
+            Console.WriteLine(obj.ToString());
+            Console.ReadKey();
         }
     }
 }
